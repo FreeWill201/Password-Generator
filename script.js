1e06fcef60dbd7f10af4d18b488e4a41645c0a97
FreeWill201 <freewill201@gmail.com>
2023-01-17 23:29:24 -0500

    First Submit Commit

--- script.js
+++ script.js
@@ -1,8 +1,86 @@
 
 
 // Assignment code here
+// Validation is down, create remaining arrays. If multiple boxes are checked created joining arrays
+// This is an array, you will need this for each option in the checkboxes
 
 
+// -My Code
+const range = document.getElementById
+('CARange')
+const number = document.getElementById
+('CANumber') 
+const UpperCase2Element = document.getElementById 
+('Uppercase')
+const Form = document.getElementById
+('PGF')
+const Numbers = document.getElementById
+('Numbers2')
+const Symbols = document.getElementById
+('Symbols')
+const Lowercase2Element = document.getElementById
+('Lowercase')
+const Password = document.getElementById
+('password')
+
+const UpperCase_Char_Codes = arrayFromLowToHigh(65, 90) 
+const LowerCase_Char_Codes = arrayFromLowToHigh(97, 122)
+const Number_Char_Codes = arrayFromLowToHigh(48, 57)
+const Symbol_Char_Codes = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+.concat(arrayFromLowToHigh(123, 126))
+
+function arrayFromLowToHigh(Low, High) {
+  const array = []
+  for (let i = Low; i <= High; i++) {
+array.push(i) 
+  }
+  return array
+}
+function generatePassword (characterAmount, includeUpperCase2, includeNumbers, 
+  includeSymbols) { 
+  let charCodes = LowerCase_Char_Codes 
+  if (includeNumbers) charCodes = charCodes.concat
+  (Number_Char_Codes)
+  if (includeSymbols) charCodes = charCodes.concat
+  (Symbol_Char_Codes)
+  if (includeUpperCase2) charCodes = charCodes.concat
+  (UpperCase_Char_Codes)
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) { 
+    const charactercode = charCodes[Math.floor(Math.random()*
+      characterAmount)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  } 
+return passwordCharacters.join('')
+}
+
+
+
+
+range.addEventListener ('input', syncCharacterAmount)
+number.addEventListener ('input', syncCharacterAmount)
+Form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const Uppercase2 = UpperCase2Element.checked
+  const Numbers = Numbers.checked
+  const Symbols = Symbols.checked
+  const password = generatePassword(CharacterAmount, Uppercase2, 
+    Numbers,Symbols) 
+Password.innerText = Password
+})
+
+// This gives appropriate interactiveness between the bar of number of characters and it's display
+function syncCharacterAmount (e) {
+  const value = e.target.value
+  number.value = value
+  range.value = value
+}
+// My Code
+
+// Starter Code
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -17,3 +95,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// Starter Code
