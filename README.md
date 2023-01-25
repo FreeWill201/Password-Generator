0a3db0f1bc112608e0c8fdcc4b7c4727467e30f2
FreeWill201 <freewill201@gmail.com>
2023-01-24 23:01:47 -0500

    Second Submit Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Password-Generator 
+
+# Link
+
+https://freewill201.github.io/Password-Generator/
+
+# Picture Of Webpage
+
+- ![ Picture Of Webpage]( images/Screenshot%202023-01-24%20at%2010.50.09%20PM.png "Webpage")
+
+# Description 
+
+This webpage aims to allow the user to generate a password based on the criteria of it being between 8-128 characters in length and whether they want to include uppercase, lowercase, symbol, or number elemenets, in any order order or combo. The user should leave the page feeling as though they have generated a safe and secure password.
+
+# Installation 
+
+The pages files can be found, each separate, on Github and inspected to better understand how a page like this is made. This information is available to anyone looking for it.
+
+# Usage
+
+This page aims to deliever a safe and secure password the user can deploy on his or her own. Each generation will be unique and very difficult to crack from a cybersecurity point of view. 
+
+# Credits
+
+VS Code and Github
+
+# License
+
+MIT License
+
+# Features
+
+Easy to understand UI. Checkboxes can be unchecked and value can be manually entered or more intuitively navigated to with the available character amount bar. 
--- script.js
+++ script.js
@@ -1,92 +1,121 @@
 
 
-// Assignment code here
-// Validation is down, create remaining arrays. If multiple boxes are checked created joining arrays
-// This is an array, you will need this for each option in the checkboxes
-
-
-// -My Code
+// Please use text wrap option in VS Code to easily read comments.
+// The code between lines 5-46 establish variables to be reused throughout the javascript section and also establishes boolean values that communicate whteher that variable is being utilized in relation to the password.
 const range = document.getElementById
 ('CARange')
-const number = document.getElementById
-('CANumber') 
-const UpperCase2Element = document.getElementById 
-('Uppercase')
-const Form = document.getElementById
-('PGF')
-const Numbers = document.getElementById
-('Numbers2')
-const Symbols = document.getElementById
-('Symbols')
-const Lowercase2Element = document.getElementById
-('Lowercase')
+var CharacterAmount= document.getElementById('CANumber').value
+console.log(CharacterAmount);
+var UpperCase2Element; 
+if (document.getElementById('Uppercase').checked="true") 
+{UpperCase2Element=true} 
+else {
+  document.getElementById('Uppercase').checked="false";
+  UpperCase2Element=false
+}
+console.log(UpperCase2Element)
+var Numbers;
+if (document.getElementById('Numbers').checked="true") 
+{Numbers=true} 
+else {
+  document.getElementById('Numbers').checked="false";
+  Numbers=false
+}
+console.log(Numbers)
+var Symbols;
+if (document.getElementById('Symbols').checked="true") 
+{Symbols=true} 
+else {
+  document.getElementById('Symbols').checked="false";
+  Symbols=false
+}
+console.log(Symbols)
+var Lowercase2Element;
+if (document.getElementById('Lowercase').checked="true") 
+{Lowercase2Element=true} 
+else {
+  document.getElementById('Lowercase').checked="false";
+  Lowercase2Element=false
+}
+console.log(Lowercase2Element)
+const Form = document.getElementById ('PGF')
 const Password = document.getElementById
 ('password')
+const Generate = document.getElementById
+('generate')
+const CharacterAmount2 = document.getElementById("CANumber")
 
-const UpperCase_Char_Codes = arrayFromLowToHigh(65, 90) 
-const LowerCase_Char_Codes = arrayFromLowToHigh(97, 122)
-const Number_Char_Codes = arrayFromLowToHigh(48, 57)
-const Symbol_Char_Codes = arrayFromLowToHigh(33, 47).concat(
-  arrayFromLowToHigh(58, 64))
-  .concat(arrayFromLowToHigh(91, 96))
-.concat(arrayFromLowToHigh(123, 126))
-
-function arrayFromLowToHigh(Low, High) {
-  const array = []
-  for (let i = Low; i <= High; i++) {
-array.push(i) 
+// The below code, lines 49-72 establsihes the generate password function which is operational as evidenced by the console.log on the line immediately following. It establishes the values within character sets that can be selected when generating a password.
+function generatePassword(CharacterAmount, UpperCase2Element, Lowercase2Element,
+  Symbols, Numbers) {
+  var charset = "";
+  if (UpperCase2Element) {
+    charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  return array
+  if (Lowercase2Element) {
+    charset += "abcdefghijklmnopqrstuvwzyx";
+  }
+  if (Symbols) {
+    charset += "!@#$%^&*()_+-={}[]:;<>/?";
+  }
+  if (Numbers) {
+    charset += "0123456789";
+  }
+  var password = "";
+  console.log(CharacterAmount)
+  for (var i = 0; i < CharacterAmount; i++) {
+    var random = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(random);
+  }
+  console.log(password)
+  return password;
 }
-function generatePassword (characterAmount, includeUpperCase2, includeNumbers, 
-  includeSymbols) { 
-  let charCodes = LowerCase_Char_Codes 
-  if (includeNumbers) charCodes = charCodes.concat
-  (Number_Char_Codes)
-  if (includeSymbols) charCodes = charCodes.concat
-  (Symbol_Char_Codes)
-  if (includeUpperCase2) charCodes = charCodes.concat
-  (UpperCase_Char_Codes)
-  const passwordCharacters = []
-  for (let i = 0; i < characterAmount; i++) { 
-    const charactercode = charCodes[Math.floor(Math.random()*
-      characterAmount)]
-    passwordCharacters.push(String.fromCharCode(characterCode))
-  } 
-return passwordCharacters.join('')
+console.log(generatePassword(12,true,true,true,true))
+
+// The below code, lines 76-89, is not funcitonal and must be amended but it aima to serve as a guide for the user if they do not enter in the required criteria for a password to be generated.
+function passwordCheck() {
+var passwordlength = range.value  
+if (isNaN(passwordlength)) {
+  alert('Please Enter Valid Number')
 }
+if (passwordlength < 8){
+  alert('Password must be at least 8 characters')
+}
+if (passwordlength > 128) {
+  alert('Password cannot exceed 128 characters')
+}
+if (UpperCase2Element === false && Numbers === false && Symbols === false && Lowercase2Element === false) {
+  alert('You must check at least one box')
+}}
+console.log(passwordCheck)
 
 
 
-
-range.addEventListener ('input', syncCharacterAmount)
-number.addEventListener ('input', syncCharacterAmount)
+// The below code, lines 95-103 help give interactivity to the number line and corresponding value in the charcter input box displayed on the webpage.
+range.addEventListener ('input', syncCharacterAmount2);
+CharacterAmount2.addEventListener ('input', syncCharacterAmount2);
 Form.addEventListener('submit', e => {
   e.preventDefault()
-  const characterAmount = characterAmountNumber.value
-  const Uppercase2 = UpperCase2Element.checked
-  const Numbers = Numbers.checked
-  const Symbols = Symbols.checked
-  const password = generatePassword(CharacterAmount, Uppercase2, 
-    Numbers,Symbols) 
+  CharacterAmount2.value
+  const Password = generatePassword(CharacterAmount2, UpperCase2Element, 
+   Numbers,Symbols, Lowercase2Element) 
 Password.innerText = Password
 })
 
-// This gives appropriate interactiveness between the bar of number of characters and it's display
-function syncCharacterAmount (e) {
+// Without this code, lines 106-110, the number of charcaters line is not interactive because it establishes the function that aligns the inputed value and the number line.
+function syncCharacterAmount2 (e) {
   const value = e.target.value
-  number.value = value
+  CharacterAmount2.value = value
   range.value = value
 }
-// My Code
 
-// Starter Code
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// This code, lines 116-123, aims to initialize the writePassword function in the same order of intial passing relative to the listed variables. 
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(CharacterAmount2, UpperCase2Element, Numbers,
+    Symbols, Lowercase2Element );
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -95,4 +124,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// Starter Code
